4055d9d5531d75987a59db2bd4d61205f119ad50
xuwei <xuwei@autelrobotics.cn>
2022-07-22 10:12:30 +0800

    update

--- xuwei_py-GPU/ddp_case1.py
+++ xuwei_py-GPU/ddp_case1.py
@@ -118,4 +118,4 @@ if __name__ == '__main__':
 ## Bash运行
 # DDP: 使用torch.distributed.launch启动DDP模式
 # 使用CUDA_VISIBLE_DEVICES，来决定使用哪些GPU
-# CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node 2 main.py
+# CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node 2 ddp_case1.py
